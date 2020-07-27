039585b66492a76e63f8fb8f92209477fe4fab5e
Dora Belme <contact.dorabelme@gmail.com>
2020-07-27 16:46:04 -0400

    project setup

---
+++ tesla-range-calculator/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import logoUrl from '../../assets/logo.svg';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img src={logoUrl} alt='Tesla'></img>
+        </div>
+    )
+}
+
+export default Header;
